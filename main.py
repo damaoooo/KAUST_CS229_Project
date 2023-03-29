fb90fb4cef78349d09a2116d0ea345d7ae6c6f59
damaoooo <damaoooo@163.com>
2023-03-29 17:53:15 +0300

    Add size control

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:100"
 
 if __name__ == "__main__":
     torch.set_float32_matmul_precision('medium')
-    model_path = "albert-base-v2"
+    model_path = "distilbert-base-uncased"
     my_model = ShortLanguageModel(lr=1e-6, model_path=model_path)
 
     my_dataset = TOEFLDataModule(batch_size=1, windows=4, use_cache="", tokenizer=model_path, subsize=0.2)
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         accelerator="gpu",
         devices=1,
         strategy="deepspeed_stage_3_offload",
-        precision=16,
+        precision="16-mixed",
         max_epochs=500
     )
     trainer.fit(model=my_model, train_dataloaders=my_dataset)
